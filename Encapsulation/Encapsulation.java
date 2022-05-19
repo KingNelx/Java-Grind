24b9dd617bd704334f12d1075bbbe0fce1898c6b
KingNelx <tapiajonel606@gmail.com>
2022-05-19 10:42:35 +0800

    update

--- Encapsulation/Encapsulation.java
+++ Encapsulation/Encapsulation.java
@@ -1,4 +1,16 @@
 package Encapsulation;
+import java.util.ArrayList;
 
+class Business{
+    private String name;
+    private String location;
+    private String type;
+    private ArrayList<String>partner;
+
+}
+
+class Partners{
+
+}
 public class Encapsulation {
 }
