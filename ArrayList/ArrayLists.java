1773291d0345c234a51b1b80bdb1c3c90e9c9edf
KingNelx <tapiajonel606@gmail.com>
2022-05-21 06:16:27 +0800

    some changes

--- ArrayList/ArrayLists.java
+++ ArrayList/ArrayLists.java
@@ -1,24 +1,7 @@
 package ArrayList;
 
-import java.util.Scanner;
-import java.util.ArrayList;
+public class ArrayLists {
+    public static void main (){
 
-public class ArrayLists{
-    public static void main (String[]args){
-        Scanner input = new Scanner(System.in);
-        ArrayList<String>emp = new ArrayList<>();
-        String names;
-        int count;
-
-        System.out.println(" How many Employees you have?: ");
-        count = input.nextInt();
-
-        for(int i = 1; i <= count; i++){
-            System.out.println(" Enter name: ");
-            names = input.nextLine();
-            emp.add(names);
-        }
-        System.out.println(emp);
     }
-
 }
