5150bdd32df00adb5713f667f195cdf6f8c71703
KingNelx <tapiajonel606@gmail.com>
2022-05-20 20:45:18 +0800

    some changes

--- Encapsulation/Encapsulation.java
+++ Encapsulation/Encapsulation.java
@@ -1,23 +1,17 @@
 package Encapsulation;
+import java.util.ArrayList;
 
 class Game{
     private String Name;
     private String Developer;
-    private String ShortDescription;
+    private String Owner;
+    private String Company;
     private String Genre;
+    private String OnlineOrOffline;
     private String Reviews;
-    private int NumberOfCopiesSell;
-    private double totalAssets;
-    public double bonusAsset = 2.5;
-
-    public void CopiesSell(){
-        if(NumberOfCopiesSell > 0){
-                totalAssets = totalAssets * bonusAsset;
-                System.out.println(" Sold some Games. Total asset is: " + totalAssets + "$");
-        }else{
-            System.out.println(" No Games has been sold for today. Total asset is: " + totalAssets + "$");
-        }
-    }
+    private double Cost;
+    private int Rating;
+    private int Duration;
 
     public void setName(String name){
         this.Name = name;
@@ -27,45 +21,15 @@ class Game{
         return Name;
     }
 
-    public void setDeveloper(String developer){
-        this.Developer = developer;
+    public void setDeveloper(String Developer){
+        this.Developer = Developer;
     }
 
     public String getDeveloper(){
         return Developer;
     }
 
-
-    public void setShortDescription(String description){
-        this.ShortDescription = description;
-    }
-
-    public String getShortDescription(){
-        return ShortDescription;
-    }
-
-    public void setGenre(String genre){
-        this.Genre = genre;
-    }
-
-    public String getGenre(){
-        return Genre;
-    }
-
-    public void setReviews(String reviews){
-        if(reviews.equals("negative")){
-
-        }
-    }
-    public void setNumberOfCopiesSell(int copies){
-        this.NumberOfCopiesSell = copies;
-    }
-
-    public int getNumberOfCopiesSell(){
-        return NumberOfCopiesSell;
-    }
-
-    public void setTotalAssets(){
+    public void setOwner(){
 
     }
 }
@@ -73,11 +37,6 @@ class Game{
 class Manager{
     String ManagerName;
 }
-
-class AnotherPerson{
-    String PersonName;
-}
-
 
 public class Encapsulation{
     public static void main (String[]args){
