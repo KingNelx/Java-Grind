096c5911a4d178211827c3d590d843d9678b2bd7
KingNelx <tapiajonel606@gmail.com>
2022-05-20 20:50:35 +0800

    some changes

--- Encapsulation/Encapsulation.java
+++ Encapsulation/Encapsulation.java
@@ -13,8 +13,8 @@ class Game{
     private int Rating;
     private int Duration;
 
-    public void setName(String name){
-        this.Name = name;
+    public void setName(String Name){
+        this.Name = Name;
     }
 
     public String getName(){
@@ -29,8 +29,68 @@ class Game{
         return Developer;
     }
 
-    public void setOwner(){
+    public void setOwner(String Owner){
+        this.Owner = Owner;
+    }
 
+    public String getOwner(){
+        return Owner;
+    }
+
+    public void setCompany(String Company){
+        this.Company = Company;
+    }
+
+    public String getCompany(){
+        return Company;
+    }
+
+    public void setGenre(String Genre){
+        this.Genre = Genre;
+    }
+
+    public String getGenre(){
+        return Genre;
+    }
+
+    public void setOnlineOrOffline(String OnlineOrOffline){
+        this.OnlineOrOffline = OnlineOrOffline;
+    }
+
+    public String getOnlineOrOffline(){
+        return OnlineOrOffline;
+    }
+
+    public void setReview(String Reviews){
+        this.Reviews = Reviews;
+    }
+
+    public String getReviews(){
+        return Reviews;
+    }
+
+    public void setCost(double Cost){
+        this.Cost = Cost;
+    }
+
+    public double getCost(){
+        return Cost;
+    }
+
+    public void setRating(int Rating){
+        this.Rating = Rating;
+    }
+
+    public int getRating(){
+        return Rating;
+    }
+
+    public void setDuration(int Duration){
+        this.Duration = Duration;
+    }
+
+    public int getDuration(){
+        return Duration;
     }
 }
 
