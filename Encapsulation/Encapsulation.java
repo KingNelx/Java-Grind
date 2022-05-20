826477f2f67c68250238a2e7d44e42768e3ffcb2
KingNelx <tapiajonel606@gmail.com>
2022-05-20 18:38:55 +0800

    some changes

--- Encapsulation/Encapsulation.java
+++ Encapsulation/Encapsulation.java
@@ -3,7 +3,19 @@ package Encapsulation;
 class Game{
     private String Name;
     private String Developer;
-    private String
+    private String ShortDescription;
+    private int NumberOfCopiesSell;
+    private double totalAssets;
+    public double bonusAsset = 2.5;
+
+    public void CopiesSell(){
+        if(NumberOfCopiesSell > 0){
+                totalAssets = totalAssets * bonusAsset;
+                System.out.println(" Sold some Games. Total asset is: " + totalAssets + "$");
+        }else{
+            System.out.println(" No Games has been sold for today. Total asset is: " + totalAssets + "$");
+        }
+    }
 }
 
 class Manager{
