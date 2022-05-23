3dc16b17f98a31daacbdb987bd43400c2de3f3a8
KingNelx <tapiajonel606@gmail.com>
2022-05-23 20:08:57 +0800

    some changes

---
+++ Inheritance/Developer.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class Developer {
+    private String name;
+    private int age;
+    private int YrsOfExperience;
+    private String position;
+    private String Company;
+    private double salary;
+}
