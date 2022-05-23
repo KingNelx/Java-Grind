07d2e90988b32b929c6b8ce064ba73df30473dc6
KingNelx <tapiajonel606@gmail.com>
2022-05-23 22:26:09 +0800

    some changes

--- Inheritance/Developer.java
+++ Inheritance/Developer.java
@@ -1,10 +1,41 @@
 package Inheritance;
 
+import java.util.Locale;
+
 public class Developer {
-    private String name;
-    private int age;
+    private String Name;
+    private int Age;
     private int YrsOfExperience;
-    private String position;
+    private String Position;
     private String Company;
-    private double salary;
+    private double Salary;
+
+    public void setPosition(String Position){
+        if(Position.equals("senior")) {
+            System.out.println(" You are a senior");
+            Salary = Salary + 500;
+        }
+        this.Position = Position;
+    }
+
+    public String getPosition(){
+        return Position;
+    }
+    public void setSalary(double Salary){
+        this.Salary = Salary;
+    }
+
+    public double getSalary(){
+        return Salary;
+    }
+
+    public void checkPosition(){
+//        if(Position.equals("senior")){
+//            System.out.println(" You are a senior");
+//            Salary = Salary + 500;
+//            System.out.println(getSalary());
+//        }
+        System.out.println(getPosition().toLowerCase(Locale.ROOT));
+        System.out.println(getSalary());
+    }
 }
--- Inheritance/Inheritance.java
+++ Inheritance/Inheritance.java
@@ -2,6 +2,9 @@ package Inheritance;
 
 public class Inheritance {
     public static void main (String[]args){
-
+        Developer s1 = new Developer();
+        s1.setSalary(500);
+        s1.setPosition("Senior");
+        s1.checkPosition();
     }
 }
