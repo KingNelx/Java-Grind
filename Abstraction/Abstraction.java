dbc60ca430272333d60737fc10c92e534ec91253
KingNelx <tapiajonel606@gmail.com>
2022-06-04 18:18:24 +0800

    update

--- Abstraction/Abstraction.java
+++ Abstraction/Abstraction.java
@@ -5,15 +5,20 @@ package Abstraction;
 }
 
 abstract class s2 {
-    public void anotherTest() {
-
-    }
+    public abstract void anotherTest();
 }
 class test implements s1{
 
     @Override
     public void testMethod() {
 
+    }
+}
+
+class tet2 extends s2 {
+    @Override
+    public void anotherTest(){
+        System.out.println(" Hi There ");
     }
 }
 public class Abstraction {
