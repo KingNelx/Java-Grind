2ef29d945c0dd71cd387867c9b1374babcacacd4
KingNelx <tapiajonel606@gmail.com>
2022-06-04 18:16:39 +0800

    update

--- Abstraction/Abstraction.java
+++ Abstraction/Abstraction.java
@@ -1,4 +1,23 @@
 package Abstraction;
 
+ interface s1{
+    public void testMethod();
+}
+
+abstract class s2 {
+    public void anotherTest() {
+
+    }
+}
+class test implements s1{
+
+    @Override
+    public void testMethod() {
+
+    }
+}
 public class Abstraction {
+    public static void main (String[]args){
+
+    }
 }
