4183fb75b959b4f9dc34b10cfabd7172672999d8
KingNelx <tapiajonel606@gmail.com>
2022-06-05 07:22:12 +0800

    update

--- AbstractionII/Company.java
+++ AbstractionII/Company.java
@@ -1,4 +1,23 @@
 package AbstractionII;
 
-public class Company {
+public abstract class Company {
+
+    private String firstName;
+    private String lastName;
+    private String CompanyName;
+    private String location;
+    private double totalAssets;
+    private double totalLiabilities;
+    public double monthlySalary;
+
+    public boolean isBankrupt(double totalAssets, double totalLiabilities){
+        return totalAssets > totalLiabilities;
+    }
+
+    public abstract void printData();
+    public abstract boolean s();
+
+
+
+
 }
